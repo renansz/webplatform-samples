2829175870804a322258088f6e0f2fc070913a8d
Renan Zanelatto <renansz@gmail.com>
2015-10-23 11:19:35 -0400

    first working version

--- webspeechdemo/hw4_agent.js
+++ webspeechdemo/hw4_agent.js
@@ -105,7 +105,7 @@ if (!('webkitSpeechRecognition' in window)) {
             start_img.src = 'mic.gif';
             showInfo('info_no_speech');
             ignore_onend = false;
-//            recognition.stop();
+            recognition.stop();
         }
         if (event.error == 'audio-capture') {
             start_img.src = 'mic.gif';
@@ -124,9 +124,9 @@ if (!('webkitSpeechRecognition' in window)) {
 
     recognition.onend = function() {
         recognizing = false;
-        if (ignore_onend) {
+/*        if (ignore_onend) {
             return;
-        }
+        }*/
         start_img.src = 'mic.gif';
         if (!final_transcript) {
             showInfo('info_start');
@@ -141,8 +141,8 @@ if (!('webkitSpeechRecognition' in window)) {
         }
 
         // CSC412 - added agent waiting condition
-        console.log("reached here in the onend function");
         if (agent_waiting){
+            console.log("reached here in the onend function. final_transcript: "+final_transcript);
             run_machine(final_transcript);
         }
     };
@@ -199,16 +199,16 @@ function showInfo(s) {
 
 var msg = new SpeechSynthesisUtterance();
 var voices = window.speechSynthesis.getVoices();
-msg.voice = voices[10]; // Note: some voices don't support altering params
+msg.voice = voices[0]; // Note: some voices don't support altering params
 msg.voiceURI = 'native';
 msg.volume = 1; // 0 to 1
 msg.rate = 0.7; // 0.1 to 10
 msg.pitch = 0.8; //0 to 2
-//msg.text = 'Hello World';
 msg.lang = 'en-US';
 
 msg.onend = function(e) {
-    console.log('Finished in ' + event.elapsedTime + ' seconds.');
+    // when finished speaking, call run_machine
+    run_machine();
 };
 
 
@@ -218,18 +218,19 @@ msg.onend = function(e) {
 var current_state = 0;
 var state; //state placeholder
 var agent_waiting = false;
+var invalid_answer_count = 0;
 
 var agent_state_machine = {
     0: {'action':'speak',
         'text':'Welcome to Internet Provider Auto Service',
         'input_map':{'null':1},
-        'screen_output':''},
+        'screen_output':'Welcome to Internet Provider Auto Service<br/><br/>'},
     1: {'action':'speak',
         'text':'Enter a menu by saying one of the listed options',
         'input_map':{'null':2},
-        'screen_output':'Main Menu:<br/>1 - Account Information<br/>2 - Techincal Support<br/>3 - Product Information<br/><br/>'},
+        'screen_output':'Main Menu:<br/>1 - Account Information<br/>2 - Technical Support<br/>3 - Product Information<br/><br/>'},
     2: {'action':'listen',
-        'input_map':{'account information':3,'technical support':'end','product_information':'end'},
+        'input_map':{'account information':3,'technical support':12,'product information':20},
         'screen_output':''},
     3: {'action':'speak',
         'text':'Account Information Selected. Choose one of the listed options',
@@ -245,7 +246,7 @@ var agent_state_machine = {
         'screen_output':''},
     6: {'action':'listen',
         'text':'',
-        'input_map':{'yes':7,'no':1},
+        'input_map':{'yes':7,'No':1},
         'screen_output':'Your current balance is 20 dollars.<br/>Do you want to pay your balance now?<br/><br/>'},
     7: {'action':'speak',
         'text':'Please provide your credit card number',
@@ -254,19 +255,74 @@ var agent_state_machine = {
     8: {'action':'listen',
         'text':'',
         'input_map':{'valid':9,'invalid':8},
-        'screen_output':'Please provide your credit card number (12 digits)<br/>1111111111111111<br/><br/>'},
+        'screen_output':'Please provide your credit card number (12 digits)<br/><br/>'},
     9: {'action':'speak',
-        'text':'Thank you for using Internet Provider auto service',
+        'text':'Please provide your credit card security code',
+        'input_map':{'null':10},
+        'screen_output':''},
+    10:{'action':'listen',
+        'text':'',
+        'input_map':{'valid':11,'invalid':10},
+        'screen_output':'Please provide your credit card security number (3 digits)<br/><br/>'},
+    11:{'action':'speak',
+        'text':'Thank you for pruchasing using Internet Provider auto service',
         'input_map':{'null':'end'},
-        'screen_output':'Thank you for using Internet Provider auto service<br/>'},
-    'end':{'action':'speak',
+        'screen_output':'Thank you for purchasing using Internet Provider auto service<br/>'},
+    12:{'action':'speak',
+        'text':'Techinical Support Selected. Please, describe your problem.',
+        'input_map':{'null':13},
+        'screen_output':'Technical Support Selected:<br/>Please, describe your problem<br/><br/>'},
+    13:{'action':'listen',
+        'input_map':{'null':14},
+        'screen_output':''},
+    14:{'action':'speak',
+        'text':'Did you try turninig it off and on?',
+        'input_map':{'null':15},
+        'screen_output':'Ok. got it.<br/>Have you tried restarting it? (yes / no)<br/><br/>'},
+    15:{'action':'listen',
+        'input_map':{'yes':19,'No':16},
+        'screen_output':''},
+    16:{'action':'speak',
+        'text':'OK. Try turning it off and on, then. Did it work?',
+        'input_map':{'null':17},
+        'screen_output':'Try turning of and on. Did it work? (yes / no)<br/><br/>'},
+    17:{'action':'listen',
+        'input_map':{'yes':18,'No':19},
+        'screen_output':''},
+    18:{'action':'speak',
+        'text':'I am glad I was able to help you out.',
+        'input_map':{'null':'end'},
+        'screen_output':'I am glad I was able to help you out.<br/><br/>'},
+    19:{'action':'speak',
+        'text':'I will transfer you to an assistant. Wait a minute, please...',
+        'input_map':{'null':'end'},
+        'screen_output':'I will transfer you to an assistant. Wait a minute, please...<br/><br/>'},
+    20:{'action':'speak',
+        'text':'These are the available products... Do you like to buy one of them?',
+        'input_map':{'null':21},
+        'screen_output':'Products:<br/>1 - Router 3000<br/>2 - Wifi Extender<br/>3 - Switch<br/>4 - Broadband Modem<br/><br/>Do you like to buy one of them?'},
+    21:{'action':'listen',
+        'input_map':{'yes':22,'no':'end'},
+        'screen_output':''},
+    22:{'action':'speak',
+        'text':'Which product are you intereseted in?',
+        'input_map':{'null':23},
+        'screen_output':'Which product do you want to buy?<br/><br/>'},
+    23:{'action':'listen',
+        'input_map':{'router 3000':7,'wifi extender':7,'switch':7,'broadband modem':7},
+        'screen_output':''},
+    24:{'action':'speak',
+        'text':'Product does not exist. Please, select one of the products listed above.',
+        'input_map':{'null':23},
+        'screen_output':'Product does not exist. Please, select one of the products listed above.<br/><br/>'},
+ 'end':{'action':'speak',
         'text':'Good bye',
         'screen_output':'Good bye<br/>'},
 }
 
-/***************************************************
+/************************************************************
     Triggers/stops listening inside the state machine
-****************************************************/
+*************************************************************/
 function startButton(event) {
     if (recognizing) {
         recognition.stop();
@@ -281,6 +337,62 @@ function startButton(event) {
     start_img.src = 'mic-slash.gif';
     showInfo('info_allow');
     start_timestamp = event.timeStamp;
+}
+
+
+/************************************************************
+    Validation functions
+*************************************************************/
+
+function product_validation(str){
+    // verify if the states are the ones waiting for products names 
+    if (current_state != 23)
+        return false;
+    
+    //if product is one of the listed, it is valid
+    if(['router 3000','wifi extender','switch','broadband modem'].indexOf(str) >= 0)
+        return true;
+    else
+        return false;
+}
+
+
+function cc_validation(str){
+    // verify if the states are the ones waiting for the credit card or csv number
+    if (!([8,10].indexOf(current_state)>=0))
+        return false;
+
+    // convert text to number, if needed
+    str.replace('one', '1');
+    str.replace('two', '2');
+    str.replace('three', '3');
+    str.replace('four', '4');
+    str.replace('five', '5');
+    str.replace('six', '6');
+    str.replace('seven', '7');
+    str.replace('eight', '8');
+    str.replace('nine', '9');
+    str.replace('zero', '0');
+
+    //test for credit cards
+    if (current_state == 10){
+        // test if it has 16 digits
+        var result = /(\d\s*){16}/.test(str);
+        if (result) return true;
+    }
+
+
+    //test for security code
+    if (current_state == 8 ){
+        // test if it has 3 digits
+        var result = /(\d\s*){3}/.test(str);
+        if (result) return true;
+    }
+
+    console.log('raw credit card number ' +str);
+
+    // if none of the above happend, it is not a valid cc / csv
+    return false;
 }
 
 /****************************************************
@@ -305,12 +417,8 @@ function run_machine(user_input){
 
             //Speak current's state text
             msg.text = state.text;
-            //speechSynthesis.speak(msg);
-            console.log(JSON.stringify(speechSynthesis.speaking));
-            while (speechSynthesis.speaking){
-                sleep(500);
-                console.log(JSON.stringify(speechSynthesis.speaking));
-            }
+            speechSynthesis.cancel();
+            speechSynthesis.speak(msg);
 
             // update Agents Text box 
             if (state.screen_output != '')
@@ -319,9 +427,9 @@ function run_machine(user_input){
             //update the next state
             current_state = state.input_map['null'];
 
-            // if new state is reached, run machine again
-            run_machine();
-
+            // the next state is ran in the onend function of the speech object 
+            // call the next state
+            //run_machine();
         }else{
             // if it is not a speak action, it has to be listen, otherwise it is an error
             assert(state.action == 'listen',"invalid action type: " + state.action);
@@ -332,50 +440,46 @@ function run_machine(user_input){
                 // update Agents Text box 
                 if (state.screen_output != '')
                     document.getElementById("agents_final_span").innerHTML += state.screen_output;
-                document.getElementById("agents_final_span").innerHTML += "<br/>Waiting for users input<br/>";
+                document.getElementById("agents_final_span").innerHTML += "<br/><i>click on the microphone to answer the question</i><br/>";
 
                 // call speech API
-                startButton(event);
+                //startButton(event);
 
                 // activate agent waiting flag
                 agent_waiting = true;
 
             }else{
-                if (user_input in state.input_map){
+                console.log("before trying to recognize:\nagent status: "+agent_waiting+" user input: "+user_input);
+                if (user_input in state.input_map || cc_validation(user_input) || product_validation(user_input) || current_state == 13){
                     agent_waiting = false;
-                    //if the answer is recognized, use it 
-                    current_state = state.input_map[user_input];
+                    invalid_answer_count = 0
+
+                    //special case on state 13, since I don't care about user's input
+                    if (current_state == 13){
+                        current_state = state.input_map['null'];
+                    }else{
+                        //if the answer was recognized, use it 
+                        current_state = state.input_map[user_input];
+                    }
 
                     document.getElementById("agents_final_span").innerHTML += "<i>User:</i> " + user_input +"<br/><br/>";
                     // go to the next state
                     run_machine();
                 }else{
-                    console.log('almost reached the right place: ' + user_input);
                     //if something was recognized but is not mapped, restart recognition
-                    document.getElementById("agents_final_span").innerHTML += "<br/>Invalid answer. Try again<br/>";
-                    startButton(event);
+                    invalid_answer_count +=1
+                    if (invalid_answer_count >=3){
+                        document.getElementById("agents_final_span").innerHTML += "<br/>Sorry, I could not understand you. I'm transfering you to an available attendant<br/>";
+                        current_state = 'end';
+                        run_machine();
+                    }else{
+                        document.getElementById("agents_final_span").innerHTML += "<br/>Invalid answer. Try again<br/>";
+                        startButton(event);
+                    }
                 }
             }
 
-
-            /*
-            switch('final transcript'){
-                case 2:
-                    current_state = state.input_map['account information'];
-                    break;
-                case 4:
-                    current_state = state.input_map['pay balance'];
-                    break;
-                case 6:
-                    current_state = state.input_map['yes'];
-                    break;
-                default:
-                    current_state = state.input_map['valid'];
-                    break;
-
-            }*/
         }
-
         //console.log('Current state'+ JSON.stringify(state));
         //console.log('Next current state: '+ current_state);
     }else{
@@ -434,25 +538,12 @@ function array_sum(arr) {
   });
 }
 /* CSC 412 - Developed functions */
-var wordCount_pieces = setInterval(speaking_rate_pieces, 2000); // 5 sec
+var wordCount_pieces = setInterval(finished_speaking, 3000); // 5 sec
 var wordCount_array = [];
 
-function speaking_rate_pieces(){
-    var delta;
+function finished_speaking(){
     if (recognizing && final_transcript != ''){
-        // push number of words to the array and gets the length of the new array
-        var array_length = wordCount_array.push(get_word_count(final_transcript));
-
-        if (array_length >= 2){
-            delta = wordCount_array[array_length-1] - wordCount_array[array_length-2];
-            // if it has passed 6 seconds and nothing happend, we assume user has finished speaking
-            if (delta == 0) recognition.stop();
-            wordCount_array = [];
-        }
+        recognition.stop();
         //debugging line
-        console.log(wordCount_array);
-    }
-    else{ //(!recognizing)
-        wordCount_array = [];
     }
 }
